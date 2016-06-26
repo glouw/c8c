ffb83c649b5442dded66b3b86d9c091d6d464ff2
Gustav Louw <yellingintothefan@gmail.com>
2016-06-26 19:01:24 +0000

    finished registers test

--- run.sh
+++ run.sh
@@ -2,24 +2,54 @@
 #   Compiles assembler and emulator
 #   Assembles and runs a chip8 program on emulator
 
+function setup {
+    clear
+    setfont ter-v12n
+}
+
+function clean {
+    printf "Press any key to continue\n"
+    read
+    clear
+    setfont ter-v16n
+}
+
+setup
+trap clean EXIT
+
 # Compile assembler and emulator
-make -s -C asm && # Assembler
-make -s -C emu    # Emulator
-if [ $? -eq 0 ]; then
-    echo "Build success!"
+make -s -C asm && # Compile assembler
+make -s -C emu    # Compile emulator
+if [ $? -eq 0 ];
+    then
+    printf "Build success\n"
+else
+    printf "Build failed\n"
+    exit
 fi
 
-# Assembler chip8 Program
-valgrind asm/asm $1.asm $1.hex && # Generate hex from asm
-xxd -r -plain $1.hex $1.bin    && # Generate bin from hex
-echo "Binary size (Bytes):"       # Print file size
-wc -c < $1.bin
-echo "Press any key to continue"
-read                              # Wait for user unput
-if [ $? -eq 0 ]; then
-    clear; setfont ter-v12n # Set font
-    valgrind emu/emu $1.bin # Execute bin
-    echo "Press any key to continue"
-    read                    # Wait for user input for valgrind output to show
-    clear; setfont ter-v16n # Revert font
+# Assemble chip8 binary
+valgrind asm/asm $1.asm $1.hex && # Assemble hex file
+xxd -r -plain $1.hex $1.bin       # Morph to bin file
+if [ $? -eq 0 ];
+    then
+    printf "Binary creation success\n"
+else
+    printf "Binary creation failed\n"
+    exit
 fi
+
+# Check binary size before continuing
+bytes=$(wc -c < $1.bin); limit=4096
+printf "%s / %s bytes\n" "$bytes" "$limit"
+if [ "$bytes" -gt "$limit" ];
+    then
+    printf "Binary is too large to fit in memory\n"
+    exit
+else
+    printf "Press any key to continue\n"
+    read
+fi
+
+# Run binary on interpreter
+valgrind emu/emu $1.bin
--- tests/register.asm
+++ tests/register.asm
@@ -66,6 +66,7 @@ register:
     DB 0x60
     DB 0x60
     DB 0x00
+
 okay:
     DB 0x3C
     DB 0x30
@@ -131,6 +132,7 @@ okay:
     DB 0x0C
     DB 0x3C
     DB 0x00
+
 fail:
     DB 0x3C
     DB 0x30
@@ -226,48 +228,135 @@ MAIN:
     LD V1, 0x00     ; y
     LD V2, 0x08     ; String length
     CALL   PRINT
-    ;
     ;   notes:
-    ;       VE is used for test failure
-    LD  VE, 0x00
+    ;       VE indicates test failure
+    LD   VE, 0x00
     ; LD Vx, byte
-    LD  V0, 0x11
-    SE  V0, 0x11
-    LD  VE, 0x01
+    LD   V0, 0x11
+    SE   V0, 0x11
+    LD   VE, 0x01
     ; LD Vx, Vy
-    LD  V1, 0x12
-    LD  V0, V1
-    SE  V0, 0x12
-    LD  VE, 0x01
+    LD   V1, 0x12
+    LD   V0, V1
+    SE   V0, 0x12
+    LD   VE, 0x01
     ; ADD Vx, byte
-    LD  V0, 0x13
-    ADD V0, 0x01
-    SE  V0, 0x14
-    LD  VE, 0x01
+    LD   V0, 0x13
+    ADD  V0, 0x01
+    SE   V0, 0x14
+    LD   VE, 0x01
     ; ADD Vx, Vy without carry
-    LD  V0, 0x15
-    LD  V1, 0x01
-    ADD V0, V1
-    SE  V0, 0x16 ; Check addition
-    LD  VE, 0x01
-    SE  VF, 0x00 ; Check carry
-    LD  VE, 0x01
+    LD   V0, 0x15
+    LD   V1, 0x01
+    ADD  V0, V1
+    SE   V0, 0x16 ; Check addition
+    LD   VE, 0x01
+    SE   VF, 0x00 ; Check carry does not occur
+    LD   VE, 0x01
     ; ADD Vx, Vy with carry
-    LD  V0, 0xFF
-    LD  V1, 0x01
-    ADD V0, V1
-    SE  V0, 0x00 ; Check addition (Overflowed)
-    LD  VE, 0x01
-    SE  VF, 0x01 ; Check carry
-    LD  VE, 0x01
+    LD   V0, 0xFF
+    LD   V1, 0x01
+    ADD  V0, V1
+    SE   V0, 0x00 ; Check addition (Overflowed)
+    LD   VE, 0x01
+    SE   VF, 0x01 ; Check carry does occur
+    LD   VE, 0x01
+    ; SUB  Vx, Vy without borrow
+    LD   V0, 0x15
+    LD   V1, 0x01
+    SUB  V0, V1
+    SE   V0, 0x14 ; Check subtraction
+    LD   VE, 0x01
+    SE   VF, 0x01 ; Check borrow does not occur
+    LD   VE, 0x01
+    ; SUB  Vx, Vy with borrow
+    LD   V0, 0x00
+    LD   V1, 0x01
+    SUB  V0, V1
+    SE   V0, 0xFF ; Check subtraction
+    LD   VE, 0x01
+    SE   VF, 0x00 ; Check borrow does occur
+    LD   VE, 0x01
+    ; SUBN Vx, Vy without borrow
+    LD   V0, 0x01
+    LD   V1, 0x15
+    SUBN V0, V1
+    SE   V0, 0x14 ; Check subtraction
+    LD   VE, 0x01
+    SE   VF, 0x01 ; Check borrow does not occur
+    LD   VE, 0x01
+    ; SUBN Vx, Vy with borrow
+    LD   V0, 0x01
+    LD   V1, 0x00
+    SUBN V0, V1
+    SE   V0, 0xFF ; Check subtraction
+    LD   VE, 0x01
+    SE   VF, 0x00 ; Check borrow does occur
+    LD   VE, 0x01
+    ; AND  Vx, Vy
+    LD   V0, 0x0F
+    LD   V1, 0xFF
+    AND  V0, V1
+    SE   V0, 0x0F ; Check AND
+    LD   VE, 0x01
+    ; OR Vx, Vy
+    LD   V0, 0x0F
+    LD   V1, 0xAA
+    OR   V0, V1
+    SE   V0, 0xAF ; Check OR
+    LD   VE, 0x01
+    ; XOR  Vx, Vy
+    LD   V0, 0x0F
+    LD   V1, 0xAA
+    XOR  V0, V1
+    SE   V0, 0xA5 ; Check XOR
+    LD   VE, 0x01
+    ; SHR  Vx, Vy without outshifting
+    LD   V0, 0x00
+    LD   V1, 0x02
+    SHR  V0, V1
+    SE   V0, 0x01 ; Check SHR
+    LD   VE, 0x01
+    SE   VF, 0x00 ; Check shifted is zero in VF
+    LD   VE, 0x01
+    ; SHR Vx, Vx with outshifting
+    SHR  V0, V0
+    SE   V0, 0x00 ; Check SHR
+    LD   VE, 0x01
+    SE   VF, 0x01 ; Check shifted is one in VF
+    LD   VE, 0x01
+    ; SHL Vx, Vy without outshifting
+    LD   V0, 0x00
+    LD   V1, 0x40
+    SHL  V0, V1
+    SE   V0, 0x80 ; Check SHL
+    LD   VE, 0x01
+    SE   VF, 0x00 ; Check shifted is zero in VF
+    LD   VE, 0x01
+    ; SHL Vx, Vx with outshifting
+    SHL  V0, V0
+    SE   V0, 0x00 ; Check SHL
+    LD   VE, 0x01
+    SE   VF, 0x01 ; Check shifted is one in VF
+    LD   VE, 0x01
+    ; RND  Vx, Byte
+    LD   V0, 0x00
+    RND  V0, 0x0F
+    LD   VF, K    ; Check with the eyes
+    LD   V0, 0x00
+    RND  V0, 0xF0
+    LD   VF, K    ; Check with the eyes
+    LD   V0, 0x00
+    RND  V0, 0xFF
+    LD   VF, K    ; Check with the eyes
     ; Print test status
-    LD  I, fail ; String
-    SE VE, 0x01
-    LD  I, okay ; String
-    LD V0, 0x00 ; x
-    LD V1, 0x08 ; y
-    LD V2, 0x08 ; String length
-    CALL   PRINT
+    LD    I, fail ; String
+    SE   VE, 0x01
+    LD    I, okay ; String
+    LD   V0, 0x00 ; x
+    LD   V1, 0x08 ; y
+    LD   V2, 0x08 ; String length
+    CALL PRINT
     ; Push any key to exit
     LD VF, K
     END
